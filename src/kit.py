0467f9b128246cb358ef01ee899da08e36736973
Yuriy Filonov <yuriy.filonov@gmail.com>
2015-10-12 18:40:33 +1100

    New method to load vocabulary only.

--- src/kit.py
+++ src/kit.py
@@ -1,5 +1,40 @@
 import log
 import struct
+import io
+
+
+def loadWord2VecVocabulary(filePath):
+    with open(filePath, 'rb') as file:
+        firstLine = file.readline()
+        embeddingsCount, embeddingSize = tuple(firstLine.split(' '))
+        embeddingsCount, embeddingSize = int(embeddingsCount), int(embeddingSize)
+        embeddingFormat = '{0}f'.format(embeddingSize)
+        wordIndexMap = {}
+
+        log.info('Vocabulary size: {0}. Embedding size: {1}.', embeddingsCount, embeddingSize)
+
+        embeddingIndex = 0
+        while True:
+            word = ''
+            while True:
+                char = file.read(1)
+
+                if not char:
+                    log.lineBreak()
+                    return wordIndexMap
+
+                if char == ' ':
+                    word = word.strip()
+                    break
+
+                word += char
+
+            file.seek(embeddingSize * 4, io.SEEK_CUR)
+            wordIndexMap[word] = len(wordIndexMap)
+            
+            embeddingIndex += 1
+            log.progress('Reading embeddings: {0:.3f}%.', embeddingIndex, embeddingsCount)
+
 
 
 def loadWord2VecEmbeddings(filePath):
