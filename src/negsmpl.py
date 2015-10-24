e670fc66e8e324ddc7c708a32fa25d01c68bcf3d
Yuriy Filonov <yuriy.filonov@gmail.com>
2015-10-24 22:39:27 +1100

    Refactoring. Decoupled model and training functionality.

--- src/negsmpl.py
+++ src/negsmpl.py
@@ -14,32 +14,30 @@ import kit
 
 
 floatX = theano.config.floatX
+empty = lambda *shape: np.empty(shape, dtype='int32')
 rnd2 = lambda d0, d1: np.random.rand(d0, d1).astype(dtype=floatX)
 
 
 class Model():
-    def __init__(self, fileIndexMap, fileEmbeddingSize, wordIndexMap, wordEmbeddings, contexts, metricsPath,
-                 epochs, batchSize, learningRate, negative, l1, l2):
-        filesCount = len(fileIndexMap)
-        wordsCount = len(wordIndexMap)
-        wordEmbeddingSize = wordEmbeddings.shape[1]
-        windowSize = contexts.shape[1] - 1
-        contextSize = windowSize - 1
+    def __init__(self, filesCount, fileEmbeddingSize, wordEmbeddings, contextSize):
+        wordsCount, wordEmbeddingSize = wordEmbeddings.shape
 
-        fileEmbeddings = theano.shared(rnd2(filesCount, fileEmbeddingSize), 'fileEmbeddings', borrow=True)
+        self.fileEmbeddings = theano.shared(rnd2(filesCount, fileEmbeddingSize), 'fileEmbeddings', borrow=True)
         wordEmbeddings = theano.shared(wordEmbeddings, 'wordEmbeddings', borrow=True)
         weights = theano.shared(rnd2(fileEmbeddingSize + contextSize * wordEmbeddingSize, wordsCount), 'weights', borrow=True)
 
-        ctxs = T.imatrix('ctxs')
-        fileIndices = ctxs[:,0:1]
-        wordIndices = ctxs[:,1:-1]
-        targetIndices = T.reshape(ctxs[:,-1:], (ctxs.shape[0],1))
+        contexts = T.imatrix('contexts')
+        fileIndices = contexts[:,0:1]
+        wordIndices = contexts[:,1:-1]
+        targetIndices = T.reshape(contexts[:,-1:], (contexts.shape[0],1))
+
+        negative = T.iscalar('negative')
 
         random = RandomStreams(0)
         negativeSampleIndices = random.random_integers((targetIndices.shape[0], negative), low=0, high=wordsCount-1)
         indices = T.concatenate([targetIndices, negativeSampleIndices], axis=1)
 
-        files = fileEmbeddings[fileIndices]
+        files = self.fileEmbeddings[fileIndices]
         fileFeatures = T.flatten(files, outdim=2)
         words = wordEmbeddings[wordIndices]
         wordFeatures = T.flatten(words, outdim=2)
@@ -49,7 +47,7 @@ class Model():
 
         probabilities = T.batched_dot(features, subWeights)
 
-        parameters = [fileEmbeddings, weights]
+        parameters = [self.fileEmbeddings, weights]
 
         l1Coefficient = T.scalar('l1Coefficient', dtype=floatX)
         l2Coefficient = T.scalar('l2Coefficient', dtype=floatX)
@@ -62,7 +60,7 @@ class Model():
 
         lr = T.scalar('learningRate', dtype=floatX)
 
-        parameters = [fileEmbeddings, weights]
+        parameters = [self.fileEmbeddings, weights]
         subParameters = [files]
         gradients = [T.grad(cost, wrt=subP) for subP in subParameters]
         updates = [(p, T.inc_subtensor(subP, -lr * g)) for p, subP, g in zip(parameters, subParameters, gradients)]
@@ -73,83 +71,21 @@ class Model():
 
         batchIndex = T.iscalar('batchIndex')
         bs = T.iscalar('batchSize')
-        trainingContexts = theano.shared(contexts, 'trainingContexts', borrow=True)
+        self.trainingContexts = theano.shared(empty(1,1), 'trainingContexts', borrow=True)
 
         self.trainModel = theano.function(
-            inputs=[batchIndex, bs, lr, l1Coefficient, l2Coefficient],
+            inputs=[batchIndex, bs, negative, lr, l1Coefficient, l2Coefficient],
             outputs=cost,
             updates=updates,
             givens={
-                ctxs: trainingContexts[batchIndex * bs : (batchIndex + 1) * bs]
+                contexts: self.trainingContexts[batchIndex * bs : (batchIndex + 1) * bs]
             }
         )
 
 
-def train(fileIndexMap, fileEmbeddingSize, wordIndexMap, wordEmbeddings, contexts, metricsPath,
+def train(model, fileIndexMap, fileEmbeddingSize, wordIndexMap, wordEmbeddings, contexts, metricsPath,
           epochs, batchSize, learningRate, negative, l1, l2):
-    filesCount = len(fileIndexMap)
-    wordsCount = len(wordIndexMap)
-    wordEmbeddingSize = wordEmbeddings.shape[1]
-    windowSize = contexts.shape[1] - 1
-    contextSize = windowSize - 1
-
-    fileEmbeddings = theano.shared(rnd2(filesCount, fileEmbeddingSize), 'fileEmbeddings', borrow=True)
-    wordEmbeddings = theano.shared(wordEmbeddings, 'wordEmbeddings', borrow=True)
-    weights = theano.shared(rnd2(fileEmbeddingSize + contextSize * wordEmbeddingSize, wordsCount), 'weights', borrow=True)
-
-    ctxs = T.imatrix('ctxs')
-    fileIndices = ctxs[:,0:1]
-    wordIndices = ctxs[:,1:-1]
-    targetIndices = T.reshape(ctxs[:,-1:], (ctxs.shape[0],1))
-
-    random = RandomStreams(0)
-    negativeSampleIndices = random.random_integers((targetIndices.shape[0], negative), low=0, high=wordsCount-1)
-    indices = T.concatenate([targetIndices, negativeSampleIndices], axis=1)
-
-    files = fileEmbeddings[fileIndices]
-    fileFeatures = T.flatten(files, outdim=2)
-    words = wordEmbeddings[wordIndices]
-    wordFeatures = T.flatten(words, outdim=2)
-    features = T.concatenate([fileFeatures, wordFeatures], axis=1)
-
-    subWeights = weights[:,indices].dimshuffle(1,0,2)
-
-    probabilities = T.batched_dot(features, subWeights)
-
-    parameters = [fileEmbeddings, weights]
-
-    l1Coefficient = T.scalar('l1Coefficient', dtype=floatX)
-    l2Coefficient = T.scalar('l2Coefficient', dtype=floatX)
-
-    l1Regularization = l1Coefficient * sum([abs(p).sum() for p in parameters])
-    l2Regularization = l2Coefficient * sum([(p ** 2).sum() for p in parameters])
-
-    cost = -T.mean(T.log(T.exp(probabilities[:,0])) + T.sum(T.log(T.exp(-probabilities[:,1:])), axis=1)) + \
-           l1Regularization + l2Regularization
-
-    lr = T.scalar('learningRate', dtype=floatX)
-
-    parameters = [fileEmbeddings, weights]
-    subParameters = [files]
-    gradients = [T.grad(cost, wrt=subP) for subP in subParameters]
-    updates = [(p, T.inc_subtensor(subP, -lr * g)) for p, subP, g in zip(parameters, subParameters, gradients)]
-
-    parameters = [weights]
-    gradients = [T.grad(cost, wrt=p) for p in parameters]
-    updates = updates + [(p, p - lr * g) for p, g in zip(parameters, gradients)]
-
-    batchIndex = T.iscalar('batchIndex')
-    bs = T.iscalar('batchSize')
-    trainingContexts = theano.shared(contexts, 'trainingContexts', borrow=True)
-
-    trainModel = theano.function(
-        inputs=[batchIndex, bs, lr, l1Coefficient, l2Coefficient],
-        outputs=cost,
-        updates=updates,
-        givens={
-            ctxs: trainingContexts[batchIndex * bs : (batchIndex + 1) * bs]
-        }
-    )
+    model.trainingContexts.set_value(contexts)
 
     startTime = time.time()
     for epoch in xrange(0, epochs):
@@ -157,11 +93,11 @@ def train(fileIndexMap, fileEmbeddingSize, wordIndexMap, wordEmbeddings, context
         batchesCount = contextsCount / batchSize + int(contextsCount % batchSize > 0)
 
         for bi in xrange(0, batchesCount):
-            error = trainModel(bi, batchSize, learningRate, l1, l2)
+            error = model.trainModel(bi, batchSize, negative, learningRate, l1, l2)
             if error <= 0:
                 break
 
-        fe = fileEmbeddings.get_value()
+        fe = model.fileEmbeddings.get_value()
         we = lambda key: fe[fileIndexMap['../data/Duplicates/Prepared/duplicates/{0}.txt'.format(key)]]
         distance = lambda left, right: vectors.euclideanDistance(we(left), we(right))
 
@@ -188,14 +124,15 @@ def train(fileIndexMap, fileEmbeddingSize, wordIndexMap, wordEmbeddings, context
         if error <= 0:
             break
 
-    validation.compareEmbeddings(fileIndexMap, fileEmbeddings.get_value())
-    # validation.plotEmbeddings(fileIndexMap, fileEmbeddings.get_value())
+    validation.compareEmbeddings(fileIndexMap, model.fileEmbeddings.get_value())
+    # validation.plotEmbeddings(fileIndexMap, model.fileEmbeddings.get_value())
 
 
 def main():
     pathTo = kit.PathTo('Duplicates')
 
     fileIndexMap = parameters.loadIndexMap(pathTo.fileIndexMap)
+    filesCount = len(fileIndexMap)
     wordIndexMap = parameters.loadIndexMap(pathTo.wordIndexMap)
     indexWordMap = parameters.loadIndexMap(pathTo.wordIndexMap, inverse=True)
     wordEmbeddings = parameters.loadEmbeddings(pathTo.wordEmbeddings)
@@ -206,10 +143,14 @@ def main():
 
     contextProvider = parameters.IndexContextProvider(pathTo.contexts)
     contexts = contextProvider[:]
+    windowSize = contexts.shape[1] - 1
+    contextSize = windowSize - 1
 
     log.info('Contexts loading complete. {0} contexts loaded {1} words each.', len(contexts), contextProvider.contextSize)
 
-    train(fileIndexMap, 200, wordIndexMap, wordEmbeddings, contexts, metricsPath,
+    model = Model(filesCount, 200, wordEmbeddings, contextSize)
+
+    train(model, fileIndexMap, 200, wordIndexMap, wordEmbeddings, contexts, metricsPath,
           epochs=5,
           batchSize=50,
           learningRate=0.01,
