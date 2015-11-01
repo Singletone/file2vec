9240d28feba9cfda563b262a829618d107220123
Yuriy Filonov <yuriy.filonov@gmail.com>
2015-11-01 22:16:33 +1100

    Started data restructuring.

--- src/injest.py
+++ src/injest.py
@@ -1,1 +1,13 @@
-__author__ = 'best'
+from infrastructure import kit
+from injestion.connectors import *
+
+
+def launch(connector, concatenatedFilePath, injestedDirPath):
+    pass
+
+if __name__ == '__main__':
+    pathTo = kit.PathTo('../data', 'Duplicates')
+
+    connector = TextFilesConnector(pathTo.dataSetDir)
+
+    launch(connector, pathTo.concatenated, pathTo.injestedDir)
--- src/injestion/connectors.py
+++ src/injestion/connectors.py
@@ -1,1 +1,2 @@
-__author__ = 'best'
+class TextFilesConnector:
+    pass
