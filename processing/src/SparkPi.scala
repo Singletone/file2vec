72b9c44d5f3af049fb30ac4b303918ce608e6f80
Yuriy Filonov <yuriy.filonov@gmail.com>
2015-11-30 20:34:30 +1100

    Removed extra folders.

--- processing/src/SparkPi.scala
+++ processing/src/SparkPi.scala
@@ -6,7 +6,7 @@ object SparkPi {
     val conf = new SparkConf().setAppName("Spark Pi").setMaster("local[4]")
     val spark = new SparkContext(conf)
     val slices = if (args.length > 0) args(0).toInt else 2
-    val n = math.min(100000L * slices, Int.MaxValue).toInt // avoid overflow
+    val n = math.min(1000000L * slices, Int.MaxValue).toInt // avoid overflow
     val count = spark.parallelize(1 until n, slices).map { i =>
       val x = random * 2 - 1
       val y = random * 2 - 1
